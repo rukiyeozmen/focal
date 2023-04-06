19c5553d2f89409f7dc3137f80406233bd33a7f9
Rukiye Ozmen <rukiiye42@gmail.com>
2023-04-06 22:26:39 +0000

    getting the length of the name usng map

---
+++ functions/map_builtin/mapTest.js
@@ -0,0 +1,6 @@
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+// write code here to use map
+// It should output: [ 15, 13, 11, 16, 10, 11, 10 ]
+
+const nameLength = lighthouses.map(name => name.length);
+console.log(nameLength);
